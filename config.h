410898bb2ded9584a90bd8f8da70a416ab1b5746
Anant Girdhar <anant.girdhar@gmail.com>
2020-06-27 15:53:52 -0400

    Add binding for switching to previous layout

--- config.h
+++ config.h
@@ -39,9 +39,9 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ "[T]=",      tile },    /* first entry is default */
 	{ "[M]",      monocle },
+	{ "><>",      NULL },    /* no layout function means floating behavior */
 };
 
 /* key definitions */
@@ -90,6 +90,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_l,      tagmon,         {.i = +1 } },
   // Layouts
 	{ MODKEY,                       XK_f,      fullscreen,     {0} },
+  { MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
@@ -97,7 +98,6 @@ static Key keys[] = {
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
 	//{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
-	//{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY,                       XK_F5,     xrdb,           {.v = NULL } },
   // System control
 	{ MODKEY|ShiftMask,             XK_x,      quit,           {0} },
